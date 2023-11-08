6f13d4d2c0fe7a588a9ffbda1c10e45fc4aa8a65
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-11-08 00:20:51 -0300

    atualizando projeto de estudos

--- loja/src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
+++ loja/src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
@@ -1,5 +1,7 @@
 package br.com.alura.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
@@ -15,6 +17,24 @@ public class ProdutoDAO {
 	
 	public void cadastrar(Produto produto) {
 		this.em.persist(produto);
+	}
+	
+	public void atualizar(Produto produto) {
+		this.em.merge(produto);
+	}
+	
+	public void remover(Produto produto) {
+		produto = em.merge(produto);
+		this.em.remove(produto);
+	}
+	
+	public Produto buscarPorId(Long id) {
+		return em.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
 	}
 
 }
--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -13,25 +14,36 @@ import br.com.alura.loja.util.JPAUtil;
 public class CadastroDeProdutos {
 
    public static void main(String[] args) {
-        Categoria celulares = new Categoria("CELULARES");
-	
-	    EntityManager em = JPAUtil.getEntityManager();
+       cadastrarProduto();
+       Long id = 1l;
+       EntityManager em = JPAUtil.getEntityManager();
+	   ProdutoDAO produtoDAO = new ProdutoDAO(em);
+	   
+	   Produto p = produtoDAO.buscarPorId(1l);
+	   System.out.println(p.getPreco());
+	   
+	   List<Produto> todos = produtoDAO.buscarTodos();
+	   todos.forEach(p2 -> System.out.println(p.getNome()));
+       
+     }
 
+	private static void cadastrarProduto() {
+	
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+	    
+	    EntityManager em = JPAUtil.getEntityManager();
+	    ProdutoDAO produtoDao = new ProdutoDAO(em);
+	    CategoriaDAO categoriaDao = new CategoriaDAO(em);
+	    
 	    em.getTransaction().begin();
 	    
-	    em.persist(celulares);
-	    celulares.setNome("XPTO");
-	     
-	    em.flush();
-	    em.clear();
+	    categoriaDao.cadastrar(celulares);
+	    produtoDao.cadastrar(celular);
 	    
-	    celulares = em.merge(celulares);
-	    celulares.setNome("1234");
-	    em.flush();
-	    em.clear();
-	    em.remove(celulares);
-	    em.flush();
-     }
+	    em.getTransaction().commit();
+	    em.close();
+	}
 
 
 }
