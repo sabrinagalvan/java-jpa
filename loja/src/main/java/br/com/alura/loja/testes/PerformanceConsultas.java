2f7b5b25e17434564cf3a23777a6a248f0950f03
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-11-21 23:11:56 -0300

    atualizando projeto de estudo

---
+++ loja/src/main/java/br/com/alura/loja/testes/PerformanceConsultas.java
@@ -0,0 +1,70 @@
+package br.com.alura.loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ClienteDAO;
+import br.com.alura.loja.dao.PedidoDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Cliente;
+import br.com.alura.loja.modelo.ItemPedido;
+import br.com.alura.loja.modelo.Pedido;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+public class PerformanceConsultas {
+
+	public static void main(String[] args) {
+		popularBancoDeDados();
+		EntityManager em = JPAUtil.getEntityManager();
+		Pedido pedido = em.find(Pedido.class, 1l);
+		System.out.println(pedido.getItens().size());
+	}
+
+	private static void popularBancoDeDados() {
+		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("8000"), videogames);
+		Produto macbook = new Produto("Macbook", "Macboo pro retina", new BigDecimal("14000"), informatica);
+
+		Cliente cliente = new Cliente("Rodrigo", "123456");
+
+		Pedido pedido = new Pedido(cliente);
+		pedido.adicionarItem(new ItemPedido(10, pedido, celular));
+		pedido.adicionarItem(new ItemPedido(40, pedido, videogame));
+
+		Pedido pedido2 = new Pedido(cliente);
+		pedido2.adicionarItem(new ItemPedido(2, pedido, macbook));
+
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+		ClienteDAO clienteDAO = new ClienteDAO(em);
+		PedidoDAO pedidoDAO = new PedidoDAO(em);
+
+		em.getTransaction().begin();
+
+		categoriaDAO.cadastrar(celulares);
+		categoriaDAO.cadastrar(videogames);
+		categoriaDAO.cadastrar(informatica);
+
+		produtoDAO.cadastrar(celular);
+		produtoDAO.cadastrar(videogame);
+		produtoDAO.cadastrar(macbook);
+
+		clienteDAO.cadastrar(cliente);
+
+		pedidoDAO.cadastrar(pedido);
+		pedidoDAO.cadastrar(pedido2);
+
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
